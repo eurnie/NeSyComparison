573111f7028b4995344ad368cf683a1d3ca082de
Arne Vermeulen <vermeulen.arne@gmail.com>
2023-05-03 20:05:58 +0200

    delete unnecessary network

--- MNIST_addition/data/network_torch.py
+++ MNIST_addition/data/network_torch.py
@@ -55,35 +55,6 @@ class Net_NN(nn.Module):
         x = x.view(-1, 16 * 11 * 4)
         x = self.classifier(x)
         return x
-
-class Net_SL(nn.Module):
-    def __init__(self, dropout_rate):
-        super(Net_SL, self).__init__()
-        self.encoder = nn.Sequential(
-            nn.Conv2d(1, 6, 5),
-            nn.MaxPool2d(2, 2),
-            nn.ReLU(True),
-            nn.Conv2d(6, 16, 5),
-            nn.MaxPool2d(2, 2),
-            nn.ReLU(True) 
-        )
-        self.classifier =  nn.Sequential(
-            nn.Dropout(p=dropout_rate),
-            # the input size is larger, because images are stacked
-            nn.Linear(16 * 11 * 4, 120), 
-            nn.ReLU(),
-            nn.Linear(120, 84),
-            nn.ReLU(),
-            # second parameter is 20, because label of 2 images are predicted
-            nn.Linear(84, 20), 
-            nn.Softmax(1)
-        )
-
-    def forward(self, x):
-        x = self.encoder(x)
-        x = x.view(-1, 16 * 11 * 4)
-        x = self.classifier(x)
-        return x
     
 class Net_NTP(nn.Module):
     def __init__(self):
