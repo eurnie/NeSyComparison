646889c6d111bf20da98d221c63321287ac75278
Arne Vermeulen <vermeulen.arne@gmail.com>
2023-05-03 20:01:44 +0200

    small fix

--- MNIST_addition/logic_tensor_networks/addition_param.py
+++ MNIST_addition/logic_tensor_networks/addition_param.py
@@ -30,132 +30,133 @@ dropout_rate = 0
 size_val = 0.1
 #########################################################################################################
 
-# setting seeds for reproducibility
-random.seed(seed)
-numpy.random.seed(seed)
-torch.manual_seed(seed)
-tf.random.set_seed(seed)
+for batch_size in [2, 4, 8, 16, 32, 64]:
+    # setting seeds for reproducibility
+    random.seed(seed)
+    numpy.random.seed(seed)
+    torch.manual_seed(seed)
+    tf.random.set_seed(seed)
 
-# generate and shuffle dataset
-if dataset == "mnist":
-    generate_dataset_mnist(seed, 0)
-elif dataset == "fashion_mnist":
-    generate_dataset_fashion_mnist(seed, 0)
+    # generate and shuffle dataset
+    if dataset == "mnist":
+        generate_dataset_mnist(seed, 0)
+    elif dataset == "fashion_mnist":
+        generate_dataset_fashion_mnist(seed, 0)
 
-# import train and val set
-train_set, val_set, _ = get_mnist_op_dataset_val(dataset, size_val, batch_size)
+    # import train and val set
+    train_set, val_set, _ = get_mnist_op_dataset_val(dataset, size_val, batch_size)
 
-# predicates
-logits_model = Net(dropout_rate)
-Digit = ltn.Predicate(ltn.utils.LogitsToPredicateModel(logits_model))
+    # predicates
+    logits_model = Net(dropout_rate)
+    Digit = ltn.Predicate(ltn.utils.LogitsToPredicateModel(logits_model))
 
-# variables
-d1 = ltn.Variable("digits1", range(10))
-d2 = ltn.Variable("digits2", range(10))
+    # variables
+    d1 = ltn.Variable("digits1", range(10))
+    d2 = ltn.Variable("digits2", range(10))
 
-# operators
-Not = ltn.Wrapper_Connective(ltn.fuzzy_ops.Not_Std())
-And = ltn.Wrapper_Connective(ltn.fuzzy_ops.And_Prod())
-Or = ltn.Wrapper_Connective(ltn.fuzzy_ops.Or_ProbSum())
-Implies = ltn.Wrapper_Connective(ltn.fuzzy_ops.Implies_Reichenbach())
-Forall = ltn.Wrapper_Quantifier(ltn.fuzzy_ops.Aggreg_pMeanError(),semantics="forall")
-Exists = ltn.Wrapper_Quantifier(ltn.fuzzy_ops.Aggreg_pMean(),semantics="exists")
+    # operators
+    Not = ltn.Wrapper_Connective(ltn.fuzzy_ops.Not_Std())
+    And = ltn.Wrapper_Connective(ltn.fuzzy_ops.And_Prod())
+    Or = ltn.Wrapper_Connective(ltn.fuzzy_ops.Or_ProbSum())
+    Implies = ltn.Wrapper_Connective(ltn.fuzzy_ops.Implies_Reichenbach())
+    Forall = ltn.Wrapper_Quantifier(ltn.fuzzy_ops.Aggreg_pMeanError(),semantics="forall")
+    Exists = ltn.Wrapper_Quantifier(ltn.fuzzy_ops.Aggreg_pMean(),semantics="exists")
 
-# mask
-add = ltn.Function.Lambda(lambda inputs: inputs[0]+inputs[1])
-equals = ltn.Predicate.Lambda(lambda inputs: inputs[0] == inputs[1])
+    # mask
+    add = ltn.Function.Lambda(lambda inputs: inputs[0]+inputs[1])
+    equals = ltn.Predicate.Lambda(lambda inputs: inputs[0] == inputs[1])
 
-# axioms
-@tf.function
-def axioms(images_x, images_y, labels_z, p_forall, p_exists):
-    images_x = ltn.Variable("x", images_x)
-    images_y = ltn.Variable("y", images_y)
-    labels_z = ltn.Variable("z", labels_z)
-    axiom = Forall(
-            ltn.diag(images_x,images_y,labels_z),
-            Exists(
-                (d1,d2),
-                And(Digit([images_x,d1]),Digit([images_y,d2])),
-                mask=equals([add([d1,d2]), labels_z]),
-                p=p_exists
-            ),
-            p=p_forall
-        )
-    sat = axiom.tensor
-    return sat
+    # axioms
+    @tf.function
+    def axioms(images_x, images_y, labels_z, p_forall, p_exists):
+        images_x = ltn.Variable("x", images_x)
+        images_y = ltn.Variable("y", images_y)
+        labels_z = ltn.Variable("z", labels_z)
+        axiom = Forall(
+                ltn.diag(images_x,images_y,labels_z),
+                Exists(
+                    (d1,d2),
+                    And(Digit([images_x,d1]),Digit([images_y,d2])),
+                    mask=equals([add([d1,d2]), labels_z]),
+                    p=p_exists
+                ),
+                p=p_forall
+            )
+        sat = axiom.tensor
+        return sat
 
-# initialize all layers
-images_x, images_y, labels_z = next(train_set.as_numpy_iterator())
-axioms(images_x, images_y, labels_z, p_forall, p_exists)
+    # initialize all layers
+    images_x, images_y, labels_z = next(train_set.as_numpy_iterator())
+    axioms(images_x, images_y, labels_z, p_forall, p_exists)
 
-# training
-optimizer = tf.keras.optimizers.Adam(learning_rate)
-metrics_dict = {
-    'train_loss': tf.keras.metrics.Mean(name="train_loss"),
-    'train_accuracy': tf.keras.metrics.Mean(name="train_accuracy"),
-    'test_loss': tf.keras.metrics.Mean(name="test_loss"),
-    'test_accuracy': tf.keras.metrics.Mean(name="test_accuracy")    
-}
-
-@tf.function
-def train_step(images_x, images_y, labels_z, p_forall, p_exists):
-    with tf.GradientTape() as tape:
-        loss = 1.- axioms(images_x, images_y, labels_z, p_forall, p_exists)
-    gradients = tape.gradient(loss, logits_model.trainable_variables)
-    optimizer.apply_gradients(zip(gradients, logits_model.trainable_variables))
-    
-@tf.function
-def test_step(images_x, images_y, labels_z):
-    predictions_x = tf.argmax(logits_model(images_x, training=False),axis=-1)
-    predictions_y = tf.argmax(logits_model(images_y, training=False),axis=-1)
-    predictions_z = predictions_x + predictions_y
-    match = tf.equal(predictions_z,tf.cast(labels_z,predictions_z.dtype))
-    metrics_dict['test_accuracy'](tf.reduce_mean(tf.cast(match,tf.float32)))
-
-best_accuracy = 0
-
-for epoch in range(nb_epochs):
     # training
-    train_modified(train_set, train_step, p_forall, p_exists, 1)
-
-    # generate name of folder that holds all the trained models
-    model_file_name = "LTN_param_{}_{}_{}_{}_{}_{}_{}_{}".format(seed, epoch + 1, batch_size, 
-            learning_rate, p_forall, p_exists, dropout_rate, size_val)
-
-    # save trained model to a file
-    with open(f'results/{dataset}/param/{model_file_name}', "wb") as handle:
-        pickle.dump(logits_model, handle, protocol=pickle.HIGHEST_PROTOCOL)
-            
-    # testing
-    accuracy = test_modified(val_set, test_step, metrics_dict)
-        
-    # save results to a summary file
-    information = {
-        "algorithm": "LTN",
-        "seed": seed,
-        "nb_epochs": epoch + 1,
-        "batch_size": batch_size,
-        "learning_rate": learning_rate,
-        "p_forall": p_forall,
-        "p_exists": p_exists,
-        "dropout_rate": dropout_rate,
-        "size_val": size_val,
-        "accuracy": float(accuracy),
-        "model_file": model_file_name
+    optimizer = tf.keras.optimizers.Adam(learning_rate)
+    metrics_dict = {
+        'train_loss': tf.keras.metrics.Mean(name="train_loss"),
+        'train_accuracy': tf.keras.metrics.Mean(name="train_accuracy"),
+        'test_loss': tf.keras.metrics.Mean(name="test_loss"),
+        'test_accuracy': tf.keras.metrics.Mean(name="test_accuracy")    
     }
-    with open(f'results/{dataset}/param/summary_param.json', "a") as outfile:
-        json.dump(information, outfile)
-        outfile.write('\n')
 
-    # print results
-    print("############################################")
-    print("Accuracy: {}".format(accuracy))
-    print("############################################")
+    @tf.function
+    def train_step(images_x, images_y, labels_z, p_forall, p_exists):
+        with tf.GradientTape() as tape:
+            loss = 1.- axioms(images_x, images_y, labels_z, p_forall, p_exists)
+        gradients = tape.gradient(loss, logits_model.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, logits_model.trainable_variables))
+        
+    @tf.function
+    def test_step(images_x, images_y, labels_z):
+        predictions_x = tf.argmax(logits_model(images_x, training=False),axis=-1)
+        predictions_y = tf.argmax(logits_model(images_y, training=False),axis=-1)
+        predictions_z = predictions_x + predictions_y
+        match = tf.equal(predictions_z,tf.cast(labels_z,predictions_z.dtype))
+        metrics_dict['test_accuracy'](tf.reduce_mean(tf.cast(match,tf.float32)))
 
-    if accuracy > best_accuracy:
-        best_accuracy = accuracy
-        counter = 0
-    else:
-        if counter >= 2:
-            break
-        counter += 1
+    best_accuracy = 0
+
+    for epoch in range(nb_epochs):
+        # training
+        train_modified(train_set, train_step, p_forall, p_exists, 1)
+
+        # generate name of folder that holds all the trained models
+        model_file_name = "LTN_param_{}_{}_{}_{}_{}_{}_{}_{}".format(seed, epoch + 1, batch_size, 
+                learning_rate, p_forall, p_exists, dropout_rate, size_val)
+
+        # save trained model to a file
+        with open(f'results/{dataset}/param/{model_file_name}', "wb") as handle:
+            pickle.dump(logits_model, handle, protocol=pickle.HIGHEST_PROTOCOL)
+                
+        # testing
+        accuracy = test_modified(val_set, test_step, metrics_dict)
+            
+        # save results to a summary file
+        information = {
+            "algorithm": "LTN",
+            "seed": seed,
+            "nb_epochs": epoch + 1,
+            "batch_size": batch_size,
+            "learning_rate": learning_rate,
+            "p_forall": p_forall,
+            "p_exists": p_exists,
+            "dropout_rate": dropout_rate,
+            "size_val": size_val,
+            "accuracy": float(accuracy),
+            "model_file": model_file_name
+        }
+        with open(f'results/{dataset}/param/summary_param.json', "a") as outfile:
+            json.dump(information, outfile)
+            outfile.write('\n')
+
+        # print results
+        print("############################################")
+        print("Accuracy: {}".format(accuracy))
+        print("############################################")
+
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+            counter = 0
+        else:
+            if counter >= 2:
+                break
+            counter += 1
