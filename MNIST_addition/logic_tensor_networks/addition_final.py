bb399221c1acc1d2a8370087fef11925b8b796a8
Arne Vermeulen <vermeulen.arne@gmail.com>
2023-05-28 16:39:39 +0200

    updates

--- MNIST_addition/logic_tensor_networks/addition_final.py
+++ MNIST_addition/logic_tensor_networks/addition_final.py
@@ -26,8 +26,8 @@ dropout_rate = 0
 size_val = 0.1
 #########################################################################################################
 
-# for dataset, label_noise in [("FashionMNIST", 0), ("MNIST", 0.1), ("MNIST", 0.25), ("MNIST", 0.5)]:
-for dataset, label_noise in [("MNIST", 0)]:
+for dataset, label_noise in [("FashionMNIST", 0), ("MNIST", 0.1), ("MNIST", 0.25), ("MNIST", 0.5)]:
+# for dataset, label_noise in [("MNIST", 0)]:
     assert (dataset == "MNIST") or (dataset == "FashionMNIST")
     for seed in range(0, 10):
         # generate name of file that holds the trained model
